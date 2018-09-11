254927af82ab6bb64bb69b9f7a08648e6eef23a8
moralesr2013 <ricardomorales4286@gmail.com>
2018-09-11 18:44:38 -0500

    added example promblems from follow along code

---
+++ Examples/JS/loggingLetters.js
@@ -0,0 +1,9 @@
+function countingCharacters(stringToCount){
+    // The length property has been mentioned in
+    // the Logging Letter Exercise in Lesson 4
+    console.log (stringToCount + " has " +
+              stringToCount.length + " characters.");
+}
+
+var portalString = "The cake is a lie";
+countingCharacters(portalString);
